7f12c64240252425c0af168118c07512e14e90e7
drom98 <diogoomarques98@gmail.com>
2019-01-29 19:51:19 +0000

    Connect to API

--- js/index.js
+++ js/index.js
@@ -1,23 +1,53 @@
 const cidade = 'Castelo Branco';
 const API_URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cidade + ',pt&appid=74ceb3b2518f00e1284ce45255274e36&units=metric&lang=pt';
 
+//Get HTML elements
+const cityNameEl = document.querySelector(".cityName");
+const iconEl = document.querySelector("img");
+const tempEl = document.querySelector(".temp");
+const descEl = document.querySelector(".description");
+
 //Fetch OpenWeatherMap API
 fetch(API_URL)
   .then(response => response.json())
   .then(result => {
-    const tmp = Math.ceil(result.main.temp);
-    const cityName = result.name;
-    const weatherDescription = result.weather[0].description;
-    const icon = result.weather[0].icon;
+    tempEl.textContent = Math.ceil(result.main.temp * 10) /10 + ("ºC");
+    cityNameEl.textContent = result.name;
+    descEl.textContent = result.weather[0].description;
+    iconEl.src = ("chrome-extension://fliffphbmddklhlnjlejgglgbofacjec/assets/icons/" + result.weather[0].icon + ".png");
   });
 
 //Get Date and Time
 const time = new Date();
 const days = ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'];
 const semana = days[time.getDay()];
-const hour = time.getHours();
-const minutes = time.getMinutes();
+let hour = time.getHours();
+let minutes = time.getMinutes();
+
+let greeting = "";
+if(hour >= 4 && hour < 12) {
+  greeting = "Bom dia, Diogo.";
+} 
+else if(hour >= 12 && hour < 18) {
+  greeting = "Boa tarde, Diogo.";
+}
+else if(hour >= 18 && hour < 20) {
+  greeting = "Boa noite, Diogo.";
+}
+else {
+  greeting = "Boa noite, Diogo.";
+}
+
+//Insert greeting
+const greetingEl = document.querySelector(".greeting");
+greetingEl.textContent = greeting;
 
 //Insert Date and Time to the DOM
-const timeElement = document.querySelector(".time");
+if(hour < 9) {
+  hour = ("0" + hour);
+}
+if(minutes < 9) {
+  minutes = ("0" + minutes);
+}
+const timeElement = document.querySelector(".date");
 timeElement.textContent = (semana + ", " + hour + ":" + minutes);
