3cba900a0f627e5a7d3b6e49885634c14d18eace
drom98 <diogoomarques98@gmail.com>
2019-03-01 19:25:14 +0000

    Update weather icon

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const weatherAPI = 'http://api.openweathermap.org/data/2.5/weather?q=' + cidade 
 
 //Get HTML elements
 const cityNameEl = document.querySelector(".location-text");
-const iconEl = document.querySelector("img");
+const iconEl = document.querySelector(".weather-icon");
 const tempEl = document.querySelector(".temp");
 const descEl = document.querySelector(".weather-info");
 
@@ -16,6 +16,7 @@ fetch(weatherAPI)
     //tempEl.textContent = Math.ceil(result.main.temp * 10) /10 + ("ºC");
     cityNameEl.textContent = result.name;
     descEl.textContent = result.weather[0].description;
+    iconEl.style.backgroundImage = "url('chrome-extension://fliffphbmddklhlnjlejgglgbofacjec/assets/icons/" + result.weather[0].icon + ".png')";
     //iconEl.src = ("chrome-extension://fliffphbmddklhlnjlejgglgbofacjec/assets/icons/" + result.weather[0].icon + ".png");
   });
 
@@ -41,39 +42,41 @@ const TokenKey = '026a9b64b04e4fb8a3bcf7fb0dac2ae0';
 const footballAPI = 'http://api.football-data.org/';
 const primeiraLiga = 2017;
 
-const fetchFootballAPI = async () => {
+function fetchFootballAPI() {
   //Fetch Primeira Liga Standings
-  await fetch(footballAPI + 'v2/competitions/' + primeiraLiga + '/standings', {
+  const response = fetch(footballAPI + 'v2/competitions/' + primeiraLiga + '/standings', {
     headers: {"X-Auth-Token" : TokenKey}
   })
-  .then(response => response.json())
-  .then(result => {
-    console.log(result);
-    const position = result.standings[0].table;
+  //const teamData = response.json();
+  
+  const position = response.standings[0].table;
+  response.forEach(element => {
+    const clubLogo = element.standings.table.team.crestUrl;
+    const clubName = element.standings.table.team.name;
+    const clubPoints = element.standings.table.points;
+    insertData(position, clubLogo, clubName, clubPoints);
+    /*
     let i = 0;
     for (i; i < position.length; i++) {
       const clubLogo = result.standings[0].table[i].team.crestUrl;
       const clubName = result.standings[0].table[i].team.name;
       const clubPoints = result.standings[0].table[i].points;
       insertData(position, i, clubLogo, clubName, clubPoints);
-    }
+    } */
   })
   .catch(error => {
     console.log('Erro: ', error);
-    document.querySelector("tbody").insertAdjacentHTML('afterbegin', error);
+    document.querySelector("table").insertAdjacentHTML('afterbegin', error);
   }); 
 }
 fetchFootballAPI();
 
 function insertData(position, i, clubLogo, clubName, clubPoints) {
-  const tableEl = document.querySelector("tbody");
+  const tableEl = document.querySelector("table");
   const posTemplate = 
     `<tr>
-      <th class="position">${position[i].position}</th>
+      <th>${position[i].position}
     </tr>
-    <tr>
-      <th>eced</th>
-    </tr>
-    `
+    <tr>${clubPoints}</tr>`
   tableEl.insertAdjacentHTML('beforeend', posTemplate);
 }
